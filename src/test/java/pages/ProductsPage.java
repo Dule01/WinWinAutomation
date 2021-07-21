f05bd27ab8673c22a08ee1c589378fa3039f8f45
Dusan <simic.dusan95@gmail.com>
2021-07-21 12:03:35 +0200

    Created method and added test for filtering a certain products by price

--- src/test/java/pages/ProductsPage.java
+++ src/test/java/pages/ProductsPage.java
@@ -2,8 +2,12 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
 
 public class ProductsPage extends CommonActions{
     WebDriver driver;
@@ -19,6 +23,15 @@ public class ProductsPage extends CommonActions{
         clickElement(driver.findElement(By.xpath("//*[@class=\"nav-primary\"]//li//span[text()=\""
                 +productCategory+"\"]/../..//span[text()=\""+productType+"\"]")));
     }
+
+    public void filterProdByMinimumPrice(String minimumPriceValue){
+        clickElement(driver.findElement(By.cssSelector(".filter-wrapper .filter-item--label [content=\""+minimumPriceValue+"\"]")));
+    }
+
+    @FindBy(css = ".col-main .category-products .item")
+    public List<WebElement> productTypeName;
+
+
 
 
 }
--- src/test/java/tests/WinWinTest.java
+++ src/test/java/tests/WinWinTest.java
@@ -1,5 +1,7 @@
 package tests;
 
+import io.qameta.allure.Description;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -13,127 +15,133 @@ public class WinWinTest extends BaseTest {
         init("CHROME", 15);
     }
 
-//    @Test(description = "Verify functionality of Search Bar")
-//    @Description("Verify that the Search Bar properly functions after entering a text value and clicking the Search Button")
-//    public void searchBarTest(){
-//        driver.get("https://www.winwin.rs/");
-//        BasePage bp = new BasePage(driver);
-//        bp.declinePushNotifications();
-//        bp.clickSearchBar();
-//        bp.searchWebSite("iPhone");
-//    }
-//
-//    @Test(description = "Verify functionality of Search Bar")
-//    @Description("Verify that the Search Bar properly functions after entering a text value and hitting the Enter " +
-//            "Button on the keyboard.")
-//    public void searchBarTestUsingEnter(){
-//        driver.get("https://www.winwin.rs/");
-//        BasePage bp = new BasePage(driver);
-//        bp.declinePushNotifications();
-//        bp.clickSearchBar();
-//        bp.searchWebSiteWithEnterKey("iPhone");
-//    }
-//
-//    @Test(description = "Verify the functionality of top links")
-//    @Description("Verify that all of the 3 links at the top of the page properly work")
-//    public void topLinksFunctionality(){
-//        driver.get("https://www.winwin.rs/");
-//        BasePage bp = new BasePage(driver);
-//        bp.declinePushNotifications();
-//        bp.clickWhereToBuyTop();
-//        bp.clickPaymentMethodsTop();
-//        bp.clickCommonQuestionsTop();
-//    }
-//
-//    @Test(description = "Header Hover Tests")
-//    @Description("Verify that all of the buttons can be properly hovered with correct display afterwards.")
-//    public void headerHoverTests() throws InterruptedException {
-//        driver.get("https://www.winwin.rs/");
-//        BasePage bp = new BasePage(driver);
-//        bp.declinePushNotifications();
-//        bp.pause(1);
-//        bp.hoverMyAcc();
-//        bp.pause(1);
-//        bp.hoverMyBasket();
-//        bp.pause(1);
-//        bp.hoverProducts();
-//        bp.pause(1);
-//    }
-//
-//    @Test(description = "Verify Help Box Links")
-//    @Description("Verify that all of the 3 help box links are clickable and functional.")
-//    public void helpBoxTest() throws InterruptedException {
-//        driver.get("https://www.winwin.rs/");
-//        BasePage bp = new BasePage(driver);
-//        bp.declinePushNotifications();
-//        bp.clickBusinessBenefits();
-//        bp.clickTaxFree();
-//        bp.clickFeedback();
-//        bp.pause(2);
-//    }
-//
-//    @Test(description = "Newsletter with an invalid email address")
-//    @Description("Verify that proper error text is displayed if an incorrect email address is entered")
-//    public void newsletterTest(){
-//        driver.get("https://www.winwin.rs/");
-//        BasePage bp = new BasePage(driver);
-//        bp.declinePushNotifications();
-//        bp.newsletterInvalidEmail("123");
-//    }
-//
-//    @Test(description = "Newsletter with a valid email address")
-//    @Description("Verify that proper error text is displayed if a correct email address is entered")
-//    public void newsletterTest2(){
-//        driver.get("https://www.winwin.rs/");
-//        BasePage bp = new BasePage(driver);
-//        bp.declinePushNotifications();
-//        bp.newsletterValidEmail("testemail12901@mail.com");
-//    }
-//
-//    @Test(description = "Verify functionality of footer links")
-//    @Description("Verify that footer links are clickable and lead to intended web page")
-//    public void footerLinks() {
-//        driver.get("https://www.winwin.rs/");
-//        BasePage bp = new BasePage(driver);
-//        bp.declinePushNotifications();
-//        bp.footerBasePageLinks("Gde kupiti");
-//        bp.footerBasePageLinks("Rokovi isporuke");
-//        bp.footerBasePageLinks("Kontakti ovlašćenih servisa ");
-//    }
-//
-//    @Test(description = "Social Links Functionality")
-//    @Description("Veirfy that all of the social link buttons are clickable and functional")
-//    public void socialLinks() throws InterruptedException {
-//        driver.get("https://www.winwin.rs/");
-//        BasePage bp = new BasePage(driver);
-//        bp.declinePushNotifications();
-//        bp.clickFacebookBtn();
-//        bp.clickInstagramBtn();
-//        bp.clickTwitterBtn();
-//        bp.clickYouTubeBtn();
-//        bp.clickWinWinBlogBtn();
-//        bp.pause(3);
-//    }
-//
-//    @Test(description = "Verify Phone Numbers")
-//    @Description("Verify that at least one of 3 contact numbers appears")
-//    public void phoneNumbersTest() throws InterruptedException {
-//        driver.get("https://www.winwin.rs/");
-//        BasePage bp = new BasePage(driver);
-//        bp.declinePushNotifications();
-//        bp.verifyPhoneNumbers();
-//    }
+    @Test(description = "Verify functionality of Search Bar")
+    @Description("Verify that the Search Bar properly functions after entering a text value and clicking the Search Button")
+    public void searchBarTest(){
+        driver.get("https://www.winwin.rs/");
+        BasePage bp = new BasePage(driver);
+        bp.declinePushNotifications();
+        bp.clickSearchBar();
+        bp.searchWebSite("iPhone");
+    }
 
-    @Test
+    @Test(description = "Verify functionality of Search Bar")
+    @Description("Verify that the Search Bar properly functions after entering a text value and hitting the Enter " +
+            "Button on the keyboard.")
+    public void searchBarTestUsingEnter(){
+        driver.get("https://www.winwin.rs/");
+        BasePage bp = new BasePage(driver);
+        bp.declinePushNotifications();
+        bp.clickSearchBar();
+        bp.searchWebSiteWithEnterKey("iPhone");
+    }
+
+    @Test(description = "Verify the functionality of top links")
+    @Description("Verify that all of the 3 links at the top of the page properly work")
+    public void topLinksFunctionality(){
+        driver.get("https://www.winwin.rs/");
+        BasePage bp = new BasePage(driver);
+        bp.declinePushNotifications();
+        bp.clickWhereToBuyTop();
+        bp.clickPaymentMethodsTop();
+        bp.clickCommonQuestionsTop();
+    }
+
+    @Test(description = "Header Hover Tests")
+    @Description("Verify that all of the buttons can be properly hovered with correct display afterwards.")
+    public void headerHoverTests() throws InterruptedException {
+        driver.get("https://www.winwin.rs/");
+        BasePage bp = new BasePage(driver);
+        bp.declinePushNotifications();
+        bp.pause(1);
+        bp.hoverMyAcc();
+        bp.pause(1);
+        bp.hoverMyBasket();
+        bp.pause(1);
+        bp.hoverProducts();
+        bp.pause(1);
+    }
+
+    @Test(description = "Verify Help Box Links")
+    @Description("Verify that all of the 3 help box links are clickable and functional.")
+    public void helpBoxTest() throws InterruptedException {
+        driver.get("https://www.winwin.rs/");
+        BasePage bp = new BasePage(driver);
+        bp.declinePushNotifications();
+        bp.clickBusinessBenefits();
+        bp.clickTaxFree();
+        bp.clickFeedback();
+        bp.pause(2);
+    }
+
+    @Test(description = "Newsletter with an invalid email address")
+    @Description("Verify that proper error text is displayed if an incorrect email address is entered")
+    public void newsletterTest(){
+        driver.get("https://www.winwin.rs/");
+        BasePage bp = new BasePage(driver);
+        bp.declinePushNotifications();
+        bp.newsletterInvalidEmail("123");
+    }
+
+    @Test(description = "Newsletter with a valid email address")
+    @Description("Verify that proper error text is displayed if a correct email address is entered")
+    public void newsletterTest2(){
+        driver.get("https://www.winwin.rs/");
+        BasePage bp = new BasePage(driver);
+        bp.declinePushNotifications();
+        bp.newsletterValidEmail("testemail12901@mail.com");
+    }
+
+    @Test(description = "Verify functionality of footer links")
+    @Description("Verify that footer links are clickable and lead to intended web page")
+    public void footerLinks() {
+        driver.get("https://www.winwin.rs/");
+        BasePage bp = new BasePage(driver);
+        bp.declinePushNotifications();
+        bp.footerBasePageLinks("Gde kupiti");
+        bp.footerBasePageLinks("Rokovi isporuke");
+        bp.footerBasePageLinks("Kontakti ovlašćenih servisa ");
+    }
+
+    @Test(description = "Social Links Functionality")
+    @Description("Veirfy that all of the social link buttons are clickable and functional")
+    public void socialLinks() throws InterruptedException {
+        driver.get("https://www.winwin.rs/");
+        BasePage bp = new BasePage(driver);
+        bp.declinePushNotifications();
+        bp.clickFacebookBtn();
+        bp.clickInstagramBtn();
+        bp.clickTwitterBtn();
+        bp.clickYouTubeBtn();
+        bp.clickWinWinBlogBtn();
+        bp.pause(3);
+    }
+
+    @Test(description = "Verify Phone Numbers")
+    @Description("Verify that at least one of 3 contact numbers appears")
+    public void phoneNumbersTest() throws InterruptedException {
+        driver.get("https://www.winwin.rs/");
+        BasePage bp = new BasePage(driver);
+        bp.declinePushNotifications();
+        bp.verifyPhoneNumbers();
+    }
+
+    @Test(description = "Product filtering by price")
+    @Description("Verify that proper product are listed after selecting them and filtering them by price")
     public void searchProducts() throws InterruptedException {
         driver.get("https://www.winwin.rs/");
         BasePage bp = new BasePage(driver);
         bp.declinePushNotifications();
         bp.hoverProducts();
         ProductsPage pp = new ProductsPage(driver);
-        pp.pause(3);
-        pp.selectProdCategoryAndType("Štampači, Office", "Bar Code i POS oprema");
-        pp.pause(3);
+        pp.pause(1);
+        pp.selectProdCategoryAndType("Laptop i tablet računari", "Laptopovi");
+        pp.pause(1);
+        pp.filterProdByMinimumPrice("50000.00");
+
+        for(int i = 0; i < pp.productTypeName.size(); i++){
+            Assert.assertTrue(pp.productTypeName.get(i).getText().contains("Laptop") || pp.productTypeName.get(i).getText().contains("laptop"));
+        }
     }
 
     @AfterMethod
