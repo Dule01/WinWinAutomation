6b94b961754702dde3ebf153e4d5b1e7824a765a
Dusan <simic.dusan95@gmail.com>
2021-07-21 13:49:25 +0200

    Created method that clicks on a random compare button on products page and one test to verify it

--- src/test/java/pages/ProductsPage.java
+++ src/test/java/pages/ProductsPage.java
@@ -6,8 +6,10 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 
 import java.util.List;
+import java.util.Random;
 
 public class ProductsPage extends CommonActions{
     WebDriver driver;
@@ -30,6 +32,24 @@ public class ProductsPage extends CommonActions{
 
     @FindBy(css = ".col-main .category-products .item")
     public List<WebElement> productTypeName;
+
+    @FindBy(css = ".products-grid .item .add-to-links .link-compare")
+    public List<WebElement> compareButtons;
+
+    @FindBy(css = ".success-msg")
+    WebElement compareSuccessMsg;
+
+
+
+    public void clickRandomCompareButton(){
+        Random randomCompareBtn = new Random();
+        int randomNum = randomCompareBtn.nextInt(compareButtons.size());
+        clickElement(compareButtons.get(randomNum));
+        System.out.println("Clicked " + randomNum);                        //showing in console that every time test is being run a new index of button is clicked
+
+        String compareSuccessText = compareSuccessMsg.getText();
+        Assert.assertTrue(compareSuccessText.contains("je dodat na listu za poređenje."));
+    }
 
 
 
